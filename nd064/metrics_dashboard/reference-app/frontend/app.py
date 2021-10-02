e3acdb6d83daeaf2633b37f033c437eb30f7bd8d
Jason Wee <peichieh@gmail.com>
2021-10-02 19:35:25 +0800

    import prometheus module

--- nd064/metrics_dashboard/reference-app/frontend/app.py
+++ nd064/metrics_dashboard/reference-app/frontend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+metrics.info("app_info", "Frontend App", version="0.0.1")
 
 @app.route('/')
 def homepage():
