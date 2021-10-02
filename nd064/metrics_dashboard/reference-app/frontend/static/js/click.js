548873c397593e805122efb7f55587964bbb285a
Jason Wee <peichieh@gmail.com>
2021-10-02 19:35:38 +0800

    just use localhost

--- nd064/metrics_dashboard/reference-app/frontend/static/js/click.js
+++ nd064/metrics_dashboard/reference-app/frontend/static/js/click.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     // all custom jQuery will go here
     $("#firstbutton").click(function () {
         $.ajax({
-            url: "http://backend.default.svc.cluster.local", success: function (result) {
+            url: "http://localhost:8081/api", success: function (result) {
                 $("#firstbutton").toggleClass("btn-primary:focus");
                 }
         });
     });
     $("#secondbutton").click(function () {
         $.ajax({
-            url: "http://trial.default.svc.cluster.local", success: function (result) {
+            url: "http://localhost:8082", success: function (result) {
                 $("#secondbutton").toggleClass("btn-primary:focus");
             }
         });
