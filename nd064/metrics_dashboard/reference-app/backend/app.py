4861619b28213cad457bdbb6e645025620bd1f58
Jason Wee <peichieh@gmail.com>
2021-10-05 22:35:05 +0800

    added debug output and prometheusmetrics mod

--- nd064/metrics_dashboard/reference-app/backend/app.py
+++ nd064/metrics_dashboard/reference-app/backend/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template, request, jsonify
 
 import pymongo
+import logging
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+
+
 
 app = Flask(__name__)
+metrics = GunicornInternalPrometheusMetrics(app)
+CORS(app)
 
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
 mongo = PyMongo(app)
-metrics = PrometheusMetrics(app)
-metrics.info("app_info", "Backend App", version="0.0.1")
+
+logging.basicConfig(level=logging.INFO)
+logging.info("enter backend app")
 
 
 
@@ -35,4 +43,4 @@ def add_star():
   return jsonify({'result' : output})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
