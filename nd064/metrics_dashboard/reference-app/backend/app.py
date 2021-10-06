efd286d4b5cdf135e06a953a085c018271fb1653
Jason Wee <peichieh@gmail.com>
2021-10-06 22:24:27 +0800

    added app trace using jaeger

--- nd064/metrics_dashboard/reference-app/backend/app.py
+++ nd064/metrics_dashboard/reference-app/backend/app.py
@@ -7,30 +7,34 @@ from flask_cors import CORS
 from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
-from opentelemetry import trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from flask_opentracing import FlaskTracing
 
 ## jaeger
-trace.set_tracer_provider(
-    TracerProvider(
-        resource=Resource.create({SERVICE_NAME: "backend-service"})
+def config_tracer():
+    config = Config(
+           config = {
+                'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name="service_backend",
+        validate=True,
+        metrics_factory=PrometheusMetricsFactory(service_name_label="service_backend")
     )
-)
-jaeger_exporter = JaegerExporter()
-span_processor = BatchSpanProcessor(jaeger_exporter)
-trace.get_tracer_provider().add_span_processor(span_processor)
-tracer = trace.get_tracer(__name__)
+    return config.initialize_tracer()
 ## jaeger
 
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app, excluded_urls="metrics")
 metrics = GunicornInternalPrometheusMetrics(app)
 CORS(app)
+
+jaeger_tracer = config_tracer()
+tracing = FlaskTracing(jaeger_tracer, False, app)
+
 
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
@@ -43,18 +47,18 @@ logging.info("enter backend app")
 
 
 @app.route('/')
+@tracing.trace()
 def homepage():
-    with tracer.start_as_current_span('helloWorld'):
-        a = "Hello World"
-    return a
+    return "Hello World"
 
 @app.route('/api')
+@tracing.trace()
 def my_api():
-    with tracer.start_as_current_span('api'):
-        answer = "something"
+    answer = "something"
     return jsonify(response=answer)
 
 @app.route('/star', methods=['POST'])
+@tracing.trace()
 def add_star():
   star = mongo.db.stars
   name = request.json['name']
