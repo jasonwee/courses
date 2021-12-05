3c24f4fe7e72c4542423acbb6bb8ba2f7a77808b
Jason Wee <peichieh@gmail.com>
2021-12-05 14:06:21 +0800

    fix path

--- nd064/udacityshop/src/adservice-v2/genhealth.sh
+++ nd064/udacityshop/src/adservice-v2/genhealth.sh
@@ -3,4 +3,6 @@
 set -e
 
 # TODO: Add the commands to generate the gRPC files
-python -m grpc_tools.protoc -I../../pb/grpc/health/v1/ --python_out=./grpc/health/v1/ --grpc_python_out=./grpc/health/v1/ health.proto
+#python -m grpc_tools.protoc -I../../pb/grpc/health/v1/ --python_out=./grpc/health/v1/ --grpc_python_out=./grpc/health/v1/ health.proto
+source py39_env/bin/activate
+python -m grpc_tools.protoc -I ../../pb/ --python_out=./grpc/health/v1 --grpc_python_out=./grpc/health/v1 grpc/health/v1/health.proto
--- nd064/udacityshop/src/adservice-v2/grpc/health/v1/health_pb2_grpc.py
+++ nd064/udacityshop/src/adservice-v2/grpc/health/v1/health_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import health_pb2 as health__pb2
+from grpc.health.v1 import health_pb2 as grpc_dot_health_dot_v1_dot_health__pb2
 
 
 class HealthStub(object):
@@ -16,8 +16,8 @@ class HealthStub(object):
         """
         self.Check = channel.unary_unary(
                 '/grpc.health.v1.Health/Check',
-                request_serializer=health__pb2.HealthCheckRequest.SerializeToString,
-                response_deserializer=health__pb2.HealthCheckResponse.FromString,
+                request_serializer=grpc_dot_health_dot_v1_dot_health__pb2.HealthCheckRequest.SerializeToString,
+                response_deserializer=grpc_dot_health_dot_v1_dot_health__pb2.HealthCheckResponse.FromString,
                 )
 
 
@@ -35,8 +35,8 @@ def add_HealthServicer_to_server(servicer, server):
     rpc_method_handlers = {
             'Check': grpc.unary_unary_rpc_method_handler(
                     servicer.Check,
-                    request_deserializer=health__pb2.HealthCheckRequest.FromString,
-                    response_serializer=health__pb2.HealthCheckResponse.SerializeToString,
+                    request_deserializer=grpc_dot_health_dot_v1_dot_health__pb2.HealthCheckRequest.FromString,
+                    response_serializer=grpc_dot_health_dot_v1_dot_health__pb2.HealthCheckResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -60,7 +60,7 @@ class Health(object):
             timeout=None,
             metadata=None):
         return grpc.experimental.unary_unary(request, target, '/grpc.health.v1.Health/Check',
-            health__pb2.HealthCheckRequest.SerializeToString,
-            health__pb2.HealthCheckResponse.FromString,
+            grpc_dot_health_dot_v1_dot_health__pb2.HealthCheckRequest.SerializeToString,
+            grpc_dot_health_dot_v1_dot_health__pb2.HealthCheckResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
